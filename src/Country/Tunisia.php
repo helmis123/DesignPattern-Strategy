410716dc448cbf4672725e596fe1d3ce1a0fb986
helmi <helmi.dridi@gmail.com>
2020-01-09 20:59:27 +0100

    Use pattern

--- src/Country/Tunisia.php
+++ src/Country/Tunisia.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Country;
+use App\Behaviors\BevaviorGetDateType2;
+
 class Tunisia
 {
     function __construct(){
         $this->hello = '3aslema';
+        $this->getDate = new BevaviorGetDateType2();
     }
 }
--- src/Country/Usa.php
+++ src/Country/Usa.php
@@ -1,13 +1,13 @@
 <?php
 namespace App\Country;
 
+use App\Behaviors\BevaviorGetDateType1;
 use App\Classes\AbstractCountry;
 
 class Usa extends AbstractCountry
 {
     function __construct(){
         $this->hello = 'Hi!';
-        $date = new \DateTime();
-        $this->date = $date->format('d/m/Y');
+        $this->getDate = new BevaviorGetDateType1();
     }
 }
